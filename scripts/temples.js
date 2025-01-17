3d125ddd79db9b24efe44f3678c7eca9aeb2e19a
rex rayla <pathway@rxedev.com>
2025-01-18 04:54:42 +0800

    week2

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+const yearSpan = document.getElementById("currentYear");
+yearSpan.textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+function toggleMenu() {
+    const menu = document.querySelector('nav ul');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
